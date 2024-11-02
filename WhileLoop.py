93a967ce5628148bcebf5e63077b4dd1b1b1cff5
sanivarapu koti reddy <kotireddy@gmail.com>
2024-11-02 10:29:34 +0530

    WhileLoop

---
+++ WhileLoop.py
@@ -0,0 +1,54 @@
+print("1.To Print a string no.of times")
+a=0
+while(a<11):
+    print("software developer")
+    a+=1
+print()
+
+print("2.false is use to print ")
+a=0
+while(False):    # false not print"
+    print("software developer")
+    a+=1
+print()
+
+print("3.print no.of strings ")
+str1=input("enter the strings:")
+print(str1)
+print()
+a=0
+while(a<len(str1)):
+    print(a)
+    a+=1
+print()
+
+print("4.print no.of numbers")
+str1=input("enter the strings:")
+print(str1)
+print()
+a=0
+while(a<len(str1)):
+    print(str1[a])
+    a+=1
+print()
+
+print("print odd numbers in a list")
+L1=[101,102,103,104,105,106,107,108,109,110,111,112,113]
+a=0
+while(a<len(L1)):
+    if (L1[a]%2==1):
+        print(L1[a])
+    a+=1
+print()
+
+print("1.To Print a string no.of times")
+a=0
+while(True):
+    print("software developer")
+    a+=1
+print()
+
+
+
+
+
