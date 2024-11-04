bb9b70b002c63c48913e0be0f592cf8aa3fda841
sanivarapu koti reddy <kotireddy@gmail.com>
2024-11-04 19:24:51 +0530

    Eval Functions

---
+++ EvalFunction.py
@@ -0,0 +1,20 @@
+
+print("adding two numbers with eval")
+x1=eval(input("enter the num of x1:"))
+x2=eval(input("enter the num of x2:"))
+result1=x1+x2
+print(result1)
+print()
+
+print("print string many times")
+x1=eval(input("enter the num of x1:"))
+x2=eval(input("enter the num of x2:"))
+result1=x1+x2
+print(result1*3)
+print()
+
+print("To print space between strings")
+x1=eval(input("enter the num of x1:"))
+print(x1)
+print()
+
