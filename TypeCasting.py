6bac7b592b27b73ed34f4273e2a3f42876357ad2
sanivarapu koti reddy <kotireddy@gmail.com>
2024-11-04 19:23:53 +0530

    Type Casting

---
+++ TypeCasting.py
@@ -0,0 +1,29 @@
+x1=int(input("enter the num of x1:"))
+x2=int(input("enter the num of x2:"))
+result1=x1+x2
+print(result1)
+print()
+
+x1=float(input("enter the num of x1:"))
+x2=float(input("enter the num of x2:"))
+result1=x1+x2
+print(result1)
+print()
+
+x1=complex(input("enter the num of x1:"))
+x2=complex(input("enter the num of x2:"))
+result1=x1+x2
+print(result1)
+print()
+
+x1=bool(input("enter the num of x1:"))
+x2=bool(input("enter the num of x2:"))
+result1=x1>x2
+print(result1)
+print()
+
+x1=str(input("enter the num of x1:"))
+x2=str(input("enter the num of x2:"))
+result1=x1+x2
+print(result1)
+print()
