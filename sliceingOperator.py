77ef789ef30b9d22b54cd01723914fb6fc651591
sanivarapu koti reddy <kotireddy@gmail.com>
2024-11-04 19:57:22 +0530

    sliceing Operator

---
+++ sliceingOperator.py
@@ -0,0 +1,67 @@
+print("syntax=str1[begin:end(end-1):step]")
+
+print("1.Form_1")
+str1="core python"
+print(str1)
+print()
+print(str1[2:])
+print()
+
+print("2.Form_1")
+str1="core python"
+print(str1)
+print()
+print(str1[7:])
+print()
+
+print("3.Form_2")
+str1="core python"
+print(str1)
+print()
+print(str1[:2])
+print()
+
+print("4.Form_2")
+str1="core python"
+print(str1)
+print()
+print(str1[:7])
+print()
+
+print("5.Form_3")
+str1="core python"
+print(str1)
+print()
+print(str1[2:7])
+print()
+
+print("6.Form_3")
+str1="core python"
+print(str1)
+print()
+print(str1[5:9])
+print()
+
+print("7.Form_4")    # "step means skiping b/w the alphabets"
+str1="core python"
+print(str1)
+print()
+print(str1[2:7:2])
+print()
+
+print("8.Form_4")    # "step means skiping b/w the alphabets"
+str1="core python"
+print(str1)
+print()
+print(str1[0:11:3])
+print()
+
+print("7.Form_5")  
+str1="core python"
+print(str1)
+print()
+print(str1[0:])
+print(str1[:])
+print(str1[::])
+print(str1[::-1])
+print()
