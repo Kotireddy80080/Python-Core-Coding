f7c608a1dcd06afa935789bc862d704126f8cee4
sanivarapu koti reddy <kotireddy@gmail.com>
2024-11-05 15:12:38 +0530

    list  properties

--- ListUsingQuestions.py
+++ ListUsingQuestions.py
@@ -133,6 +133,62 @@ s1.clear()
 print(s1)
 print()
 
+print("1.insertinting into a list")
+l1=[1,2,3,5,4,6,7,9,8]
+print(l1)
+print(type(l1))
+print()
+for i in l1:
+    print(i)
+print()
 
+print("2.duplicates are allowed into a list") # same as above program
+
+print()
+
+print("3.hetegrogenes objects are allowed into a list") # same as above program
+print()
+
+print("4.list is mutable example")
+l1=["a","b","c","d"]
+print(l1)
+print(type(l1))
+l1[0]="a  for apple"
+l1[1]="b for ball"
+l1[2]="c for cat"
+l1[3]="d for dog"
+print(l1)
+print(type(l1))
+print()
+
+print("5.dynamic or growable")
+l1=[]
+print(l1)
+print("add values")
+l1.append(1001)
+l1.append("koti")
+l1.append(102.2)
+print(l1)
+print()
+print("remove values")
+l1.remove(1001)
+l1.remove("koti")
+l1.remove(102.2)
+print(l1)
+print()
+
+print("indexing and sliceing")
+l1=[1001,"koti",42000,"hcl"]
+print(l1)
+print("indexing")
+print("eid is:",l1[0])
+print("name is:",l1[1])
+print("salary is:",l1[2])
+print("company is:",l1[3])
+print()
+print("sliceing")
+print(l1[0:])
+print(l1[::-1])
+print()
 
 
