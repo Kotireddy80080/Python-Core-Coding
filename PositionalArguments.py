44f2fd66a824979f1cb305ec946519946fd624bb
sanivarapu koti reddy <kotireddy@gmail.com>
2024-12-12 11:44:02 +0530

    Positional arguments

---
+++ PositionalArguments.py
@@ -0,0 +1,29 @@
+print("positional arguments")   # combination of actual and formal parameters
+print("example -1")
+import time 
+def Test_Case1(x1,x2,x3):
+    print("The value of x1 is:",x1)
+    print()
+    print("The value of x2 is:",x2)
+    print()
+    print("The value of x3 is:",x3)
+if(__name__=="__main__"):
+    Test_Case1(1001,1002,1003)
+print()
+time.sleep(2)
+print('End of an application')
+
+
+print("example -2")
+import time 
+def Test_Case1(x3,x2,x1):
+    print("The value of x1 is:",x1)
+    print()
+    print("The value of x2 is:",x2)
+    print()
+    print("The value of x3 is:",x3)
+if(__name__=="__main__"):
+    Test_Case1(1001,1002,1003)
+print()
+time.sleep(2)
+print('End of an application')
