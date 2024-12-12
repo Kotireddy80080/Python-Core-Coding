064b39f7e22a9cf2c9ab9554e9a2cd9b794a2793
sanivarapu koti reddy <kotireddy@gmail.com>
2024-12-12 11:41:02 +0530

    functional programming

---
+++ FunctionalProgramming.py
@@ -0,0 +1,25 @@
+print("Functional programming lang")  # piece of code and code reusability which means once write call any number of times
+
+print("example -1")
+import time 
+def Test_Case1():
+    print("Welcome to functional programming language in Python")
+if(__name__=="__main__"):
+    Test_Case1()
+print()
+time.sleep(3)
+print("End of an application")
+
+print("example -2")
+import time 
+def Test_Case1():
+    print("Welcome to functional programming language in Python")
+if(__name__=="__main__"):
+    Test_Case1()
+    print()
+    Test_Case1()
+    print()
+    Test_Case1()
+print()
+time.sleep(3)
+print("End of an application")
